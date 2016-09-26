51663fb0fc9731e822dbb0c3793b7894693e18e2
tejaswigowda <tejaswi@foxyninjastudios.com>
2016-09-26 10:08:33 -0700

    node hw

--- nodeHW/hw.js
+++ nodeHW/hw.js
@@ -1,9 +1,9 @@
-const http = require('http');
+var http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var hostname = '127.0.0.1';
+var port = 3000;
 
-const server = http.createServer((req, res) => {
+var server = http.createServer(function(req, res){
     res.statusCode = 200;
       res.setHeader('Content-Type', 'text/plain');
         res.end('Hello World\n');
