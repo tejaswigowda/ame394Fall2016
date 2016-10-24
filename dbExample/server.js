35e26e128e5b00eeb577e3be4911fda0ab4e7063
tejaswigowda <tejaswi@foxyninjastudios.com>
2016-10-24 09:55:29 -0700

    in class

---
+++ dbExample/server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 8080;
+var db = require('./node_modules/mongoskin').db('mongodb://user:password@127.0.0.1:27017/RSSFEED');
+
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
