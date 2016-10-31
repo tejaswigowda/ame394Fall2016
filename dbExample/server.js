3a921b4217b15c4c75edc4baafdc77d013b818e6
EC2 Default User <ec2-user@ip-172-30-1-109.us-west-2.compute.internal>
2016-10-31 17:14:28 +0000

    in class

--- dbExample/server.js
+++ dbExample/server.js
@@ -10,6 +10,7 @@ var db = require('./node_modules/mongoskin').db('mongodb://user:pwd@127.0.0.1:27
 
 app.get('/addSensorData', function(req, res){
   var info = req.query;
+	info.ts = new Date().getTime();
       db.collection('data').insert(info, function(err3, r3) {
         if (err3) {
           res.send("0");            
@@ -18,6 +19,26 @@ app.get('/addSensorData', function(req, res){
           res.send("1");
         }   
       });
+});
+
+
+app.get('/getData', function(req, res){
+  var info = req.query;
+	var start  = parseInt(info.startTime);
+      db.collection('data').find({ts:{$gte:start}}).limit(20).toArray(function(err, result){
+		console.log(err);
+		console.log(result);
+	  if(err){
+		res.send("[]")
+	  }
+	  else{
+		res.send(JSON.stringify(result));
+	  }
+	});
+
+
+
+
 });
 
 
