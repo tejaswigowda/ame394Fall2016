b8180ddc8ba21556c6126d389c8288495fa5a14b
tejaswigowda <tejaswi@foxyninjastudios.com>
2016-11-21 09:23:32 -0700

    ac

---
+++ InternetMessaging/server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 8080;
+var reading = 0;
+
+
+app.get('/setReading', function(req, res){
+  var info = req.query;
+  reading = info.r;
+  res.send("1");
+});
+
+
+app.get('/getReading', function(req, res){
+  res.send(reading);
+});
+
+
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
