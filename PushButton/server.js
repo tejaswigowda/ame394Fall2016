5de62a35dcde97ff5adecca033c56fa483e0f433
tejaswigowda <tejaswi@foxyninjastudios.com>
2016-11-30 09:41:16 -0700

    in class

---
+++ PushButton/server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 8080;
+var reading = 0;
+
+app.get('/buttonPressed', function(req, res){
+  var info = req.query;
+  if(reading == 1){
+    reading = 0;
+  }
+  else{
+    reading = 1;
+  }
+  res.send("1");
+});
+app.get('/getState', function(req, res){
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.write(reading);
+  res.end()
+
+});
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
